a2443504bdd3c97174012a9237feb72376771855
jeh <hodellurt@gmail.com>
2022-10-15 14:26:01 +0900

    feat(lencodable): vencodable

--- src/lencodable.lean
+++ src/lencodable.lean
@@ -7,9 +7,7 @@ class lencodable (Γ : Type*) (α : Type*) :=
 (decode [] : list Γ → option α)
 (encodek : ∀ a, decode (encode a) = some a)
 
-abbreviation bencodable := lencodable bool
-
-notation `⸤`x`⸥` := lencodable.encode bool x
+--notation `⸤`x`⸥` := lencodable.encode bool x
 
 attribute [simp] lencodable.encodek
 
@@ -29,7 +27,7 @@ variables (Γ)
 
 def length (a : α) : ℕ := (encode Γ a).length
 
-notation `∥`a`∥` := length bool a
+--notation `∥`a`∥` := length bool a
 
 instance refl : lencodable Γ Γ :=
 { encode := λ b, [b],
@@ -56,121 +54,134 @@ instance : lencodable Γ pempty :=
 
 end lencodable
 
-class lfin (Γ : Type*) (α : Type*) extends lencodable Γ α :=
+class vencodable (Γ : Type*) (α : Type*) :=
 (entropy [] : ℕ)
-(entropy_ne_zero : entropy ≠ 0)
-(length : ∀ a : α, lencodable.length Γ a = entropy)
+(encode [] : α → vector Γ entropy)
+(decode [] : vector Γ entropy → option α)
+(encodek : ∀ a, decode (encode a) = some a)
 
-abbreviation bfin := lfin bool
+attribute [simp] vencodable.encodek
 
-abbreviation bentropy := lfin.entropy bool
+abbreviation bencodable := vencodable bool
+abbreviation bentropy := vencodable.entropy bool
 
-attribute [simp] lfin.length lfin.entropy_ne_zero
+namespace vencodable
+variables {Γ : Type*} {α : Type*} [vencodable Γ α]
 
-namespace lfin
-variables {Γ : Type*} {α : Type*} [lfin Γ α]
+notation `∥`α`∥` := entropy bool α
 
-@[simp] lemma length' : ∀ a : α, (lencodable.encode Γ a).length = lfin.entropy Γ α := lfin.length 
+notation `⸤`x`⸥` := encode bool x
 
-@[simp] lemma length_encode : list.length ∘ (lencodable.encode Γ : α → list Γ) = (λ _, lfin.entropy Γ α) := funext (by simp)
+lemma encode_injective  : function.injective (@encode Γ α _)
+| x y e := option.some.inj $ by rw [← @encodek Γ, e, encodek]
 
-variables (Γ α)
+@[simp] lemma encode_inj {a b : α} : encode Γ a = encode Γ b ↔ a = b :=
+encode_injective.eq_iff
 
-def entropy' := (entropy Γ α).pred
+@[simp] lemma decode_comp_encode :
+  (decode : vector Γ (entropy Γ α)  → option α) ∘ (encode Γ : α → vector Γ (entropy Γ α)) = some :=
+funext (by simp)
 
-@[simp] lemma entropy_lt_zero : 0 < entropy Γ α := pos_iff_ne_zero.mpr entropy_ne_zero
+variables (Γ)
 
-@[simp] lemma entropy'_succ_eq_entropy : (entropy' Γ α).succ = entropy Γ α :=
-nat.succ_pred_eq_of_pos (by simp)
+def length (a : α) : ℕ := (encode Γ a).length
 
-@[simp] lemma entropy'_lt : entropy' Γ α < entropy Γ α := by rw[←entropy'_succ_eq_entropy]; exact lt_add_one _
+instance refl : vencodable Γ Γ :=
+{ entropy := 1,
+  encode := λ b, ⟨[b], by simp⟩,
+  decode := λ d, d.nth 0,
+  encodek := λ b, by refl }
 
-variables {α}
+instance refll (k : ℕ) : vencodable Γ (vector Γ k) :=
+{ entropy := k,
+  encode := id,
+  decode := some,
+  encodek := by simp }
 
-def encode' : α → vector Γ (entropy Γ α) := λ a, ⟨lencodable.encode Γ a, by simp⟩
+variables {Γ}
 
-lemma encode'_nth (a : α) (n) : (encode' Γ a).nth n = (lencodable.encode Γ a).nth_le n (by simp; exact n.property) :=
-vector.nth_eq_nth_le _ _
+instance : vencodable Γ pempty :=
+{ entropy := 0, 
+  encode := by rintros ⟨⟩,
+  decode := λ _, none,
+  encodek := by rintros ⟨⟩ }
 
-end lfin
+instance : vencodable Γ unit :=
+{ entropy := 0, 
+  encode := λ u, vector.nil,
+  decode := λ x, some (),
+  encodek := by rintros ⟨⟩; refl }
 
-abbreviation bentropy' := lfin.entropy' bool
+variables {α} {β : Type*} (e : β ≃ α)
 
-namespace lencodable
-open lfin
-variables (Γ : Type*) (Δ : Type*) (E : Type*)
+def of_equiv : vencodable Γ β :=
+{ entropy := entropy Γ α,
+  encode := λ b, encode Γ (e.to_fun b),
+  decode := λ d, (decode d).map e.inv_fun,
+  encodek := λ b, by simp }
 
-instance : lfin Γ Γ := ⟨1, by simp, by simp[length]⟩
+@[simp] lemma of_equiv_entropy_eq : (of_equiv e : vencodable Γ β).entropy = entropy Γ α := rfl
 
-def trans [lencodable Δ Γ] [lfin E Δ] : lencodable E Γ :=
-{ encode := λ x, (encode Δ x).bind (encode E),
-  decode := λ d, decode Γ ((d.to_chunks (lfin.entropy E Δ)).filter_map (decode Δ)),
-  encodek := λ x, by { 
-    have : ((encode Δ x).bind (encode E)).to_chunks (lfin.entropy E Δ) = (encode Δ x).map (encode E),
-    from list.join_to_chunks (by simp) (by simp), 
-    have : (list.map (encode E) (encode Δ x)).filter_map (decode Δ) = encode Δ x, by simp[list.filter_map_map],
-    simp* } }
-
-def trans_lfin [lfin Δ Γ] [lfin E Δ] : lfin E Γ :=
-{ entropy := entropy E Δ * entropy Δ Γ,
-  entropy_ne_zero := by simp,
-  length := λ x, by { show ((encode Δ x).bind (encode E)).length = entropy E Δ * entropy Δ Γ, by simp },
-  .. trans Γ Δ E }
-
-
-end lencodable
+end vencodable
 
 namespace nat
 attribute [simp] bit_zero bodd_bit div2_bit nat.div2_one nat.div2_two
+open lencodable
+
+def dibit : ℕ → 𝔹 := binary_rec [] (λ b n d, b :: d)
+
+namespace dibit
 
 @[simp] lemma bit_tt_zero : bit tt 0 = 1 := rfl
 
-def to_dibit : ℕ → 𝔹 := binary_rec [] (λ b n d, b :: d)
 -- 逆順の二進数表示
--- 注意 to_dibit 0 = []
+-- 注意 dibit 0 = []
 
-def to_dibit_inv : 𝔹 → ℕ
+def inv : 𝔹 → ℕ
 | [] := 0
-| (b :: d) := bit b (to_dibit_inv d)
+| (b :: d) := bit b (inv d)
+
+@[simp] lemma bit_ff_two_mul (n : ℕ) : bit ff n = 2 * n := bit0_eq_two_mul n
+
+@[simp] lemma bit_tt_two_mul_add_one (n : ℕ) : bit tt n = 2 * n + 1 := by simpa[bit, bit1] using bit0_eq_two_mul n
+
+lemma inv_length_le : ∀ d : 𝔹, inv d + 1 ≤ 2^d.length
+| [] := by simp[inv]
+| (b :: d) := by { simp[inv],
+    have IH : inv d + 1 ≤ 2^d.length, from inv_length_le d,
+    calc
+      bit b (inv d) + 1 ≤ 2 * (inv d) + 1 + 1 : by rcases b; simp
+                    ... = 2 * (inv d + 1)     : by ring
+                    ... ≤ 2 * (2^d.length)    : by simpa using IH
+                    ... ≤ 2^(d.length + 1)    : by simp[pow_add, mul_comm] }
+
+lemma inv_length_lt (d : 𝔹) : inv d < 2^d.length := succ_le_iff.mp (inv_length_le d)
 
 @[simp] lemma bit_bodd {b n} : (bit b n).bodd = b :=
 by cases b; simp[bit]
 
-@[simp] lemma to_dibit_bit_0 : to_dibit 0 = [] := by simp[to_dibit]
+@[simp] lemma dibit_0 : dibit 0 = [] := by simp[dibit]
 
-@[simp] lemma to_dibit_bit_1 : to_dibit 1 = [tt] :=
-by { unfold to_dibit binary_rec, simp, rw nat.div2_one, simp }
+@[simp] lemma dibit_1 : dibit 1 = [tt] :=
+by { unfold dibit binary_rec, simp, rw nat.div2_one, simp }
 
-@[simp] lemma to_dibit_bit {b n} (h : bit b n ≠ 0) : to_dibit (bit b n) = b :: to_dibit n :=
+@[simp] lemma dibit_bit {b n} (h : bit b n ≠ 0) : dibit (bit b n) = b :: dibit n :=
 begin
-  suffices : dite (bit b n = 0) (λ _, []) _ = b :: to_dibit n,
-  { unfold to_dibit binary_rec at this ⊢, exact this },
+  suffices : dite (bit b n = 0) (λ _, []) _ = b :: dibit n,
+  { unfold dibit binary_rec at this ⊢, exact this },
   simp[h], rw nat.div2_bit b n, refl
 end
 
-@[simp] lemma to_dibit_inv_to_divit : ∀ n, to_dibit_inv (to_dibit n) = n :=
-binary_rec (by simp[to_dibit, to_dibit_inv])
-(λ b n h, by { by_cases hn : bit b n = 0; simp[hn, to_dibit_inv, h] })
+@[simp] lemma dibit_inv_to_divit : ∀ n, inv (dibit n) = n :=
+binary_rec (by simp[dibit, inv])
+(λ b n h, by { by_cases hn : bit b n = 0; simp[hn, inv, h] })
 
-instance : bencodable ℕ :=
-{ encode := to_dibit,
-  decode := λ d, some (to_dibit_inv d),
-  encodek := by simp }
-
-def log2 : ℕ → ℕ := log 2
-
-lemma encode_bit {b n} (h : bit b n ≠ 0) : ⸤bit b n⸥ = b :: ⸤n⸥ := to_dibit_bit h
-
-@[simp] lemma encode_0 : ⸤(0 : ℕ)⸥ = [] := to_dibit_bit_0
-
-@[simp] lemma encode_1 : ⸤(1 : ℕ)⸥ = [tt] := to_dibit_bit_1
-
-lemma dibit_length : ∀ n, n ≠ 0 → ∥n∥ = log 2 n + 1 :=
+lemma dibit_length : ∀ n, n ≠ 0 → (dibit n).length = log 2 n + 1 :=
 binary_rec (by simp)
 (λ b n h, by {
   by_cases hn : n = 0; simp[lencodable.length, hn] at h ⊢,
   { rcases b; simp },
-  { intros nezero, simp [encode_bit nezero],
+  { intros nezero, simp [dibit_bit nezero],
     have : log 2 n + 1 = log 2 (bit b n),
       calc log 2 n + 1 = log 2 (bit b n / 2) + 1
         : by rw (show bit b n / 2 = n, by simpa[nat.div2_val] using div2_bit b n)
@@ -178,4 +189,95 @@ binary_rec (by simp)
         : by { symmetry, refine nat.log_of_one_lt_of_le (by simp) (by rcases b; simp[bit, one_le_iff_ne_zero.mpr hn]) },
     simpa[this] using h } }) 
 
+@[simp] lemma dibit_length_le (n) : (dibit n).length ≤ log 2 n + 1 :=
+by by_cases C : n = 0; simp[C, dibit_length]
+
+example (n : ℕ) : bit1 n = 2* n + 1 := bit1_val n
+
+lemma dibit_inv_append :
+  ∀ d₁ d₂, inv (d₁ ++ d₂) = inv d₁ + 2^d₁.length * inv d₂
+| []         d₂ := by simp[inv]
+| (tt :: d₁) d₂ := by {
+    have : inv (d₁ ++ d₂) = inv d₁ + 2^d₁.length * inv d₂ := dibit_inv_append d₁ d₂,
+    simp[inv, this, bit, bit1_val],
+    calc  2 * (inv d₁ + 2^d₁.length * inv d₂) + 1
+        = 2 * inv d₁ + 2^(d₁.length + 1) * inv d₂ + 1
+    : by simp[mul_add, pow_add, ←mul_assoc, mul_comm]
+    ... = 2 * inv d₁ + 1 + 2^(d₁.length + 1) * inv d₂
+    : by ring }
+| (ff :: d₁) d₂ := by {
+    have : inv (d₁ ++ d₂) = inv d₁ + 2^d₁.length * inv d₂ := dibit_inv_append d₁ d₂,
+    simp[inv, this, bit, bit0_val (inv d₁ + _), bit0_val (inv d₁)], 
+    show 2*(inv d₁ + 2^d₁.length * inv d₂)
+       = 2*inv d₁ + 2^(d₁.length + 1) * inv d₂,
+    by simp[mul_add, pow_add, ←mul_assoc, mul_comm] }
+
+@[simp] lemma dibit_inv_repeat_ff : ∀ n, inv (list.repeat ff n) = 0
+| 0       := by simp[inv]
+| (n + 1) := by simp[inv, bit, dibit_inv_repeat_ff n]
+
+@[simp] lemma dibit_inv_append_repeat (d) (n) : inv (d ++ list.repeat ff n) = inv d :=
+by simp[dibit_inv_append]
+
+def lift (d : 𝔹) {n : ℕ} (h : d.length ≤ n) : 𝕓 n := ⟨d ++ list.repeat ff (n - d.length), by simp[h]⟩
+
+@[simp] def dibit_inv_dibit_lift (d : 𝔹) (n : ℕ) (h : d.length ≤ n) : inv (lift d h).val = inv d :=
+by simp[lift]
+
+end dibit
+
+instance : lencodable bool ℕ :=
+{ encode := dibit,
+  decode := λ d, some (dibit.inv d),
+  encodek := by simp }
+
+def log2 : ℕ → ℕ := log 2
+
+@[simp] lemma encode_0 : encode bool (0 : ℕ) = [] := dibit.dibit_0
+
+@[simp] lemma encode_1 : encode bool (1 : ℕ) = [tt] := dibit.dibit_1
+
+lemma dibit_length : ∀ n, n ≠ 0 → length bool n = log 2 n + 1 := dibit.dibit_length
+
 end nat
+
+namespace fin
+open nat nat.dibit
+variables {n : ℕ}
+
+def bencode (i : fin n) : 𝕓 (log 2 n + 1) := 
+lift (dibit i) (by {
+  calc
+    (dibit i).length ≤ log 2 i + 1 : dibit_length_le i
+                 ... ≤ log 2 n + 1: by simp; exact log_monotone (le_of_lt i.property) })
+
+def bdecode (d : 𝕓 (log 2 n + 1)) : option (fin n) := 
+if h : inv d.val < n then some ⟨inv d.val, h⟩ else none
+
+#eval dibit 0
+#eval dibit 1
+#eval dibit 2
+#eval dibit 3
+#eval log 2 3 + 1
+
+instance : bencodable (fin n) :=
+{ entropy := log 2 n + 1,
+  encode := bencode,
+  decode := bdecode,
+  encodek := λ ⟨i, h⟩, by simp[bencode, bdecode, dibit.lift, h] }
+
+@[simp] lemma entropy_eq : ∥fin n∥ = log 2 n + 1 := rfl
+
+end fin
+
+namespace vencodable
+open nat
+variables (α : Type*) [fintype α]
+
+noncomputable def of_fin : bencodable α := of_equiv (fintype.equiv_fin α)
+
+lemma of_fin_entropy_eq : (of_fin α : bencodable α).entropy = log 2 (fintype.card α) + 1 := rfl
+
+
+
+end vencodable
